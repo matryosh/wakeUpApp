ddb0488c84c71c41684e9f9d952e5e5a85a1d30c
matryosh <tbrick1234@gmail.com>
2016-12-31 18:31:36 -0500

    I did something today. That should count.

--- main.py
+++ main.py
@@ -9,14 +9,22 @@ class SetTimer(BoxLayout):
     nap_time = NumericProperty
 
     def check_time(self):
-        if 12 <= self.nap_time.text <= 20:
+        if 12 <= int(self.nap_time.text) <= 20:
+            #print(self.nap_time.text)
             return True
-        return False
+        else:
+            print("Pick a number between 12 and 20.")
+            return False
 
     def countdown(self):
-        time = int(self.nap_time.text)*60
-        for i in range(0,time+1):
-            print(i)
+        time = int(self.nap_time.text)
+        if self.check_time():
+            i = time * 60
+            while i >= 0:
+                count_label = str(i/60)+":00" if i % 60 == 0 else str(i/60)+":"+str(i%60)
+                print count_label
+                i -= 1
+
 
     def testButton(self):
         print("Just seeing if the button works.")
