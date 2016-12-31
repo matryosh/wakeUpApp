8104d59dca48c4c2019943293865a25a5da4ab75
Trevor <tbrick1234@gmail.com>
2016-12-31 01:15:30 -0500

    imported time class sleep function

--- main.py
+++ main.py
@@ -3,7 +3,7 @@ kivy.require('1.9.1')
 from kivy.app import App
 from kivy.uix.boxlayout import BoxLayout
 from kivy.properties import NumericProperty, ObjectProperty
-
+from time import sleep
 class SetTimer(BoxLayout):
 
     nap_time = NumericProperty
@@ -14,7 +14,8 @@ class Countdown(BoxLayout):
     nap_timer = NumericProperty
 
     def startTime(self):
-        print self.nap_timer.text
+        time = int(self.nap_timer.text[:2])*60
+        print(time)
 
 class TimerRoot(BoxLayout):
     startTimer = ObjectProperty()
@@ -22,6 +23,7 @@ class TimerRoot(BoxLayout):
     def startTimer(self):
         self.clear_widgets()
         self.add_widget(Countdown())
+        Countdown().startTime()
 
     def setTimer(self):
         self.clear_widgets()
